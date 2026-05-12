4d906a028fb7fcd934d0aacc8b986558cc355cb0
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2026-05-12 21:29:06 +0430

    feat: added font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
     theme: {
         extend: {
             fontFamily: {
-                vazirmatn: ["Vazirmatn", "Roboto"],
+                iranyekan: ["Iranyekan", "Roboto"],
             },
 
             colors: {
