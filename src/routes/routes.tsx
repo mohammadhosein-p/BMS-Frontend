1d09fda148579de41e0ff64328a7242f3e6c37dd
Ali <alinaghinjad@gmail.com>
2026-05-27 09:33:42 +0330

    add Member managment page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -9,6 +9,7 @@ import Error404 from "@/pages/Error404";
 import ProfileTab from "@/pages/home/profileTab";
 import { homeItems } from "@/config/homeItems";
 import RoleGuard from "@/components/home/RoleGaurd";
+import ManagerUsers from "@/pages/home/ManagerUsers";
 
 export const router = createBrowserRouter([
     {
